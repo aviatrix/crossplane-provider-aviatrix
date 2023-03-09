20ac6085ad09eec3ae0b52d8e0244edd696d02f0
Mitch Connors <mconnors@aviatrix.com>
2023-03-09 20:35:51 +0000

    extra prep

--- apis/zz_register.go
+++ apis/zz_register.go
@@ -10,7 +10,7 @@ package apis
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 
-	v1alpha1 "github.com/aviatrix/provider-aviatrix/apis/null/v1alpha1"
+	// v1alpha1 "github.com/aviatrix/provider-aviatrix/apis/null/v1alpha1"
 	v1alpha1apis "github.com/aviatrix/provider-aviatrix/apis/v1alpha1"
 	v1beta1 "github.com/aviatrix/provider-aviatrix/apis/v1beta1"
 )
@@ -18,7 +18,7 @@ import (
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes,
-		v1alpha1.SchemeBuilder.AddToScheme,
+		// v1alpha1.SchemeBuilder.AddToScheme,
 		v1alpha1apis.SchemeBuilder.AddToScheme,
 		v1beta1.SchemeBuilder.AddToScheme,
 	)
--- internal/controller/zz_setup.go
+++ internal/controller/zz_setup.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/upbound/upjet/pkg/controller"
 
-	resource "github.com/aviatrix/provider-aviatrix/internal/controller/null/resource"
+	// resource "github.com/aviatrix/provider-aviatrix/internal/controller/null/resource"
 	providerconfig "github.com/aviatrix/provider-aviatrix/internal/controller/providerconfig"
 )
 
@@ -17,7 +17,7 @@ import (
 // the supplied manager.
 func Setup(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
-		resource.Setup,
+		// resource.Setup,
 		providerconfig.Setup,
 	} {
 		if err := setup(mgr, o); err != nil {
