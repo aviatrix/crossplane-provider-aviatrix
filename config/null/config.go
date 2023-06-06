4a1751630a52319edd51696624275dda490e2195
Mitch Connors <mitchconnors@gmail.com>
2023-06-06 18:52:18 +0000

    move version fields to status

--- config/null/config.go
+++ config/null/config.go
@@ -5,13 +5,18 @@ Copyright 2021 Upbound Inc.
 package null
 
 import (
+	"github.com/upbound/upjet/pkg/config"
 	ujconfig "github.com/upbound/upjet/pkg/config"
 )
 
 // Configure configures the null group
 func Configure(p *ujconfig.Provider) {
-	p.AddResourceConfigurator("null_resource", func(r *ujconfig.Resource) {
-		r.Kind = "Resource"
-		// And other overrides.
+	p.AddResourceConfigurator("aviatrix_gateway", func(r *config.Resource) {
+		// r.LateInitializer = config.LateInitializer{
+		// 	IgnoredFields: []string{"software_version"},
+
+		// }
+		config.MoveToStatus(r.TerraformResource, "software_version")
+		config.MoveToStatus(r.TerraformResource, "image_version")
 	})
 }
